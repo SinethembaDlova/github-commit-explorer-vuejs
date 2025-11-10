58ebfbdf2b280008bf12d474f8a4c8139e948346
SinethembaDlova <dsinethemba@gmail.com>
2025-11-11 01:36:05 +0200

    feat(repo view): fetched all repositories when we have a use. Also fetch users when a user changes.

--- src/views/Repo.vue
+++ src/views/Repo.vue
@@ -9,4 +9,23 @@
 
   // Get username from route params
   const username = computed(() => route.params.username)
+
+  // Lifecycle - Fetch repositories when component mounts or username changes
+  watch(
+    username,
+    (newUsername) => {
+      if (newUsername) {
+        githubStore.fetchRepositories(newUsername)
+        githubStore.resetCommits()
+        githubStore.setSelectedRepo(null)
+      }
+    },
+    { immediate: true }
+  )
+
+  onMounted(() => {
+    if (username.value) {
+      githubStore.fetchRepositories(username.value)
+    }
+  })
 </script>
